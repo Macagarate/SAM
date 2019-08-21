5e9bfa02193c7a3e38717693cb9db18d577d0517
tomateasd <38660954+tomateasd@users.noreply.github.com>
2019-08-20 22:57:13 -0400

    Update settings.py

--- app/sam/sam/settings.py
+++ app/sam/sam/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'sam.wsgi.application'
 DATABASES = {
 
     'default': {
-         'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.mysql',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'l8zzdmssju9468vf',
-        'HOST': 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        'USER': 'dhtcear20b26dof6',
-        'PASSWORD':'zwnmwmzxth6ynt9i',
+        'NAME': 'proyecto_sam',
+        'HOST': 'localhost',
+        'USER': 'sam',
+        'PASSWORD':'',
         'PORT': '3306'
     }
 
